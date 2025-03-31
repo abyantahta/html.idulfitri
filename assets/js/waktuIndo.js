39dd3ba8851d7b27e9874428073d8635b56529b1
abyantahta <abyantahta2002@gmail.com>
2025-03-31 11:04:34 +0700

    idul fitri

---
+++ assets/js/waktuIndo.js
@@ -0,0 +1,62 @@
+
+const monthID = () => {
+  if (month == 0) {
+    return 'Januari'
+  }
+  if (month == 1) {
+    return 'Februari'
+  }
+  if (month == 2) {
+    return 'Maret'
+  }
+  if (month == 3) {
+    return 'April'
+  }
+  if (month == 4) {
+    return 'Mei'
+  }
+  if (month == 5) {
+    return 'Juni'
+  }
+  if (month == 6) {
+    return 'Juli'
+  }
+  if (month == 7) {
+    return 'Agustus'
+  }
+  if (month == 8) {
+    return 'September'
+  }
+  if (month == 9) {
+    return 'Oktober'
+  }
+  if (month == 10) {
+    return 'November'
+  }
+  if (month == 11) {
+    return 'Desember'
+  }
+}
+const dayID = () => {
+  if (day == 0) {
+    return 'Senin'
+  }
+  if (day == 1) {
+    return 'Selasa'
+  }
+  if (day == 2) {
+    return 'Rabu'
+  }
+  if (day == 3) {
+    return 'Kamis'
+  }
+  if (day == 4) {
+    return 'Jumat'
+  }
+  if (day == 5) {
+    return 'Sabtu'
+  }
+  if (day == 6) {
+    return 'Minggu'
+  }
+}
